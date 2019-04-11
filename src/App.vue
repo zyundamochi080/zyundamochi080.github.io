29b12d7af81935e4db955deb7c9c9008f071b0fe
KG <kg.vitaty14@gmail.com>
2019-04-11 22:19:03 +0900

    Update various

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Primary title
+            </h1>
+            <h2 class="subtitle">
+              Primary subtitle
+            </h2>
+
+            <div class="columns">
+              <div class="columun">
+                <a class="button">Small</a>
+              </div>
+              <div class="columun">
+                <a class="button">Small</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <div class="columns">
+        <div class="columun">
+          <a class="button">Small</a>
+        </div>
+        <div class="columun">
+          <a class="button">Small</a>
+        </div>
+      </div>
+    </header>
+
+    <ul>
+    <li>
+      <router-link to="/">Top</router-link>
+    </li>
+    <li>
+      <router-link to="about">About</router-link>
+    </li>
+    <li>
+      <router-link to="note">Note</router-link>
+    </li>
+    <li>
+      <router-link to="api">Api</router-link>
+    </li>
+    <li>
+      <router-link to="contact">Contact</router-link>
+    </li>
+    <li>
+      <router-link to="application">application</router-link>
+    </li>
+    </ul>
   </div>
 </template>
 
@@ -11,13 +64,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import top from '@/components/top'
+import about from '@/components/about'
+import note from '@/components/note'
+import application from '@/components/application'
 
 Vue.use(Router)
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'top',
+      component: top
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about
+    },
+    {
+      path: '/note',
+      name: 'note',
+      component: note
+    },
+    {
+      path: '/application',
+      name: 'application',
+      component: application
     }
   ]
 })
